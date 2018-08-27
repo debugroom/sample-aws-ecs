eeeab753b5134e86c047c6eeb69a229c41239b1c
Kohei Kawabata <kohei.kawabata@mac.com>
2018-08-27 15:24:15 +0900

    Modify application launch error.

--- sample-aws-ecs-backend/src/main/java/org/debugroom/sample/aws/ecs/backend/config/WebApp.java
+++ sample-aws-ecs-backend/src/main/java/org/debugroom/sample/aws/ecs/backend/config/WebApp.java
@@ -3,7 +3,9 @@ package org.debugroom.sample.aws.ecs.backend.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+
+@SpringBootApplication(exclude={ElastiCacheAutoConfiguration.class})
 public class WebApp {
 
     public static void main(String[] args) {
--- sample-aws-ecs-frontend/src/main/java/org/debugroom/sample/aws/ecs/frontend/config/WebApp.java
+++ sample-aws-ecs-frontend/src/main/java/org/debugroom/sample/aws/ecs/frontend/config/WebApp.java
@@ -3,9 +3,10 @@ package org.debugroom.sample.aws.ecs.frontend.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
 
 @EnableConfigurationProperties
-@SpringBootApplication
+@SpringBootApplication(exclude={ElastiCacheAutoConfiguration.class})
 public class WebApp {
 
     public static void main(String[] args) {
